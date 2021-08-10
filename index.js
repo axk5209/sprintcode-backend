7e2ca70d440f89fba38e29938b00fb8fe86867c3
axk5209 <arhamkothari@utexas.edu>
2021-08-10 15:23:47 -0500

    update judge0 configuration

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.json())
 app.use(cors())
 
 const requestLogger = (request, response, next) => {
-	// console.log('Method:', request.method)
-	// console.log('Path:  ', request.path)
-	// console.log('Body:  ', request.body)
-	// console.log('---')
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
 	next()
 }
 const languages = {
@@ -31,45 +31,25 @@ app.use(requestLogger)
 
 app.post('/api/compile', async (request, response) => {
 	const body = request.body
-	// console.log(body)
 	const check = {
 		"language_id": languages[body.language],
 		"source_code": body.source_code,
 		"stdin": body.stdin
 	}
-	// console.log(check)
 	const postResponse = await axios({
 		"method": "POST",
-		"url": "https://judge0.p.rapidapi.com/submissions",
+		"url": "https://judge0-ce.p.rapidapi.com/submissions",
 		"headers": {
 			"content-type": "application/json",
-			"x-rapidapi-host": "judge0.p.rapidapi.com",
-			"x-rapidapi-key": "9b0f31e01fmsh859f41ecd1db8bap19b89fjsn60e1e45356ff",
+			"x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+			"x-rapidapi-key": "b3f157d6cdmsh8510bfb6b4f15e8p1f0521jsnf2227fcc230a",
 			"accept": "application/json",
 			"useQueryString": true
 		}, "data": check
 	})
 
-	// response.json({token: postResponse.data.token})
 	const token = postResponse.data.token
 	response.json(token)
-	// let getResponse = await responseLoop(token)
-	// console.log(getResponse.data)
-
-	// console.log(token)
-	// setTimeout(async () => {
-	// 	const getResponse = await axios({
-	// 		"method": "GET",
-	// 		"url": `https://judge0.p.rapidapi.com/submissions/${token}`,
-	// 		"headers": {
-	// 			"content-type": "application/octet-stream",
-	// 			"x-rapidapi-host": "judge0.p.rapidapi.com",
-	// 			"x-rapidapi-key": "9b0f31e01fmsh859f41ecd1db8bap19b89fjsn60e1e45356ff",
-	// 			"useQueryString": true
-	// 		}
-	// 	})
-	// 	console.log(getResponse)
-	// }, 5000)	
 })
 
 app.get('/api/:token', async (request, response) => {
@@ -78,11 +58,11 @@ app.get('/api/:token', async (request, response) => {
 	{
 		let getResponse = await axios({
 				"method": "GET",
-				"url": `https://judge0.p.rapidapi.com/submissions/${token}`,
+				"url": `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
 				"headers": {
 					"content-type": "application/octet-stream",
-					"x-rapidapi-host": "judge0.p.rapidapi.com",
-					"x-rapidapi-key": "9b0f31e01fmsh859f41ecd1db8bap19b89fjsn60e1e45356ff",
+					"x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+					"x-rapidapi-key": "b3f157d6cdmsh8510bfb6b4f15e8p1f0521jsnf2227fcc230a",
 					"useQueryString": true
 				}
 			})
@@ -96,15 +76,11 @@ app.get('/api/:token', async (request, response) => {
 		{
 			setTimeout(async () => {
 				await responseLoop(token);
-				//console.log(tokenResponse.data);
 			}, 1000)
 		}
 		else 
 		{
-			//console.log(tokenResponse.data)
-			//console.log("sending")
 			response.json(tokenResponse.data)
-			//console.log("Still Here for some reason")
 		}
 	}
 	await responseLoop(token)
@@ -116,74 +92,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
-
-
-
-/*
-statusText: 'Created',
-  headers: {
-    'cache-control': 'max-age=0, private, must-revalidate',
-    'content-type': 'application/json; charset=utf-8',
-    date: 'Tue, 04 Aug 2020 20:33:24 GMT',
-    etag: 'W/"92449077d6aa34de5ca165fe7314ac52"',
-    server: 'RapidAPI-1.1.24',
-    'strict-transport-security': 'max-age=31536000',
-    vary: 'Origin',
-    'x-rapidapi-region': 'AWS - us-east-1',
-    'x-rapidapi-version': '1.1.24',
-    'x-ratelimit-individual-submissions-limit': '10000',
-    'x-ratelimit-individual-submissions-remaining': '9980',
-    'x-request-id': '57c04b16-2e84-438a-bb7f-1ee9912dc707',
-    'x-runtime': '0.036570',
-    'content-length': '48',
-    connection: 'Close'
-  },
-  config: {
-    url: 'https://judge0.p.rapidapi.com/submissions',
-    method: 'post',
-    data: '{"language_id":50,"source_code":"#include <stdio.h>\\n\\nint main(void) {\\n  char name[10];\\n  scanf(\\"%s\\",name);\\n  printf(\\"hello %s\\\\n\\",name);\\n  return 0;\\n}","stdin":"world"}',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'x-rapidapi-host': 'judge0.p.rapidapi.com',
-	  'x-rapidapi-key': '9b0f31e01fmsh859f41ecd1db8bap19b89fjsn60e1e45356ff',
-	  useQueryString: true,
-      'User-Agent': 'axios/0.19.2',
-      'Content-Length': 179
-    },
-
-
-	response: {
-    status: 422,
-    statusText: 'Unprocessable Entity',
-    headers: {
-      'cache-control': 'no-cache',
-      'content-type': 'application/json; charset=utf-8',
-      date: 'Tue, 04 Aug 2020 20:41:58 GMT',
-      server: 'RapidAPI-1.1.24',
-      'strict-transport-security': 'max-age=31536000',
-      vary: 'Origin',
-      'x-rapidapi-region': 'AWS - us-east-1',
-      'x-rapidapi-version': '1.1.24',
-      'x-ratelimit-individual-submissions-limit': '10000',
-      'x-ratelimit-individual-submissions-remaining': '9979',
-      'x-request-id': '0c7e82be-c6e3-4f00-bbd6-be3ed10d059e',
-      'x-runtime': '0.119687',
-      'content-length': '78',
-      connection: 'Close'
-    },
-    config: {
-      url: 'https://judge0.p.rapidapi.com/submissions',
-      method: 'post',
-      data: '{"language_id":25,"source_code":"#include <stdio.h>\\n\\nint main(void) {\\n  char name[10];\\n  scanf(\\"%s\\", name);\\n  printf(\\"hello %s\\\\n\\", name);\\n  return 0;\\n}","stdin":"world"}',
-      headers: [Object],
-      transformRequest: [Array],
-      transformResponse: [Array],
-      timeout: 0,
-      adapter: [Function: httpAdapter],
-      xsrfCookieName: 'XSRF-TOKEN',
-      xsrfHeaderName: 'X-XSRF-TOKEN',
-      maxContentLength: -1,
-      validateStatus: [Function: validateStatus]
-    },
-*/
